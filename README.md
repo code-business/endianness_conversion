ab144effca46557503abdcfd7eedf256306c7716
gautam031098 <gomzi98@gmail.com>
2021-06-23 11:06:59 +0530

    endianness_conversion package

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # endianness_conversion
+
+A package for rearranging hexadecimal string based on endianness and data type.
+
+**endianness_conversion APIs**
+
+1. _getFormattedEndianness(hexString,endianness)_
+
+   Rearranges hexadecimal string based on endianness.Returns a string.
+
+   **Parameters**
+
+   - hexString: hexadecimal string
+   - endianness: order of hexadecimal digits needed in output.It can take the following values:
+
+   **Example**
+
+   ```
+   const { getFormattedEndianness } = require('endianness_conversion');
+
+   let hexString_F = "1326485b";
+   let endianness_F = "Little Endian"; //descending order
+   let result_F = getFormattedEndianness(hexString_F, endianness_F);
+
+   console.log("result_F = ",result_F);
+   ```
+
+   **Output**
+
+   result_F = 5b482613
---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ * Rearranges hexadecimal string based on value of endianness
+ * @param {string} hexString - hexadecimal string
+ * @param {string} endianness - order of hexadecimal bytes needed in output
+ * @returns {string} hexadecimal string rearranged based on endianness and data type
+ */
+ const getFormattedEndianness = (hexArray, endianness) => {
+    let mapper;
+  
+    //apply switch case based on value of endianness to get rearranged hexadecimal string
+    if (hexArray.length === 4) {
+      switch (endianness) {
+        case "Mid-Little Endian":
+          mapper = [2, 3, 0, 1];
+          return mapper.map((i) => hexArray[i]).join("");
+        case "Little Endian":
+          mapper = [3, 2, 1, 0];
+          return mapper.map((i) => hexArray[i]).join("");
+        case "Mid-Big Endian":
+          mapper = [1, 0, 3, 2];
+          return mapper.map((i) => hexArray[i]).join("");
+        //For any other case(Eg. Big Endian), we return the string as it is
+        default:
+          return hexArray.join("");
+      }
+    }
+    else if (hexArray.length === 8) {
+      switch (endianness) {
+        case "Mid-Little Endian":
+          mapper = [6, 7, 4, 5, 2, 3, 0, 1];
+          return mapper.map((i) => hexArray[i]).join("");
+        case "Little Endian":
+          mapper = [7, 6, 5, 4, 3, 2, 1, 0];
+          return mapper.map((i) => hexArray[i]).join("");
+        case "Mid-Big Endian":
+          mapper = [5, 4, 7, 6, 1, 0, 3, 2];
+          return mapper.map((i) => hexArray[i]).join("");
+        default:
+          return hexArray.join("");
+      }
+    } else {
+      return hexArray.join("")
+    }
+  };
+  
+  /**
+   * @module endianness_conversion
+   */
+  module.exports = { getFormattedEndianness };
+  
